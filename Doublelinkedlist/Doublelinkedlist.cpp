59754ce84d60d3c8fc856665739d2f595348fde3
Davinn354 <daviipad53@gmail.com>
2024-05-05 06:22:42 +0700

    Penambahan while

--- Doublelinkedlist/Doublelinkedlist.cpp
+++ Doublelinkedlist/Doublelinkedlist.cpp
@@ -39,6 +39,35 @@ void addNode()
     }
     else
     {
-        
+        // insert the new node in the middle or at the end
+        Node* current = START; //step 1.a: start from the first node
+        Node* previous = NULL; // step 1.b: previous node is NULL initially
+        while (current != NULL && current->noMhs < newNode->noMhs)
+        {                       // step 1.c: traverse the list to find the correct position
+            previous = current; // step 1.d: move the previous to the current node
+            current = current->next; // step 1.e: move the current to the next node
+
+        }
+
+        newNode->next = current; // step 4: make tthe nest field of the new node point to current
+        newNode->prev = previous; // step 5: make the previous field of the new node point to previous'
+
+        if (current != NULL)
+        {
+            current->prev = newNode; // step 6: make the previous field of the current node point to the new nod
+
+        }
+
+        if (previous != NULL)
+        {
+            previous->next = newNode; // step 7: make the next field of the previous node point to the new node
+
+        }
+        else
+        {
+            // if previous is still NULL, it means newNode is now the first node
+            START = newNode;
+
+        }
     }
 }
